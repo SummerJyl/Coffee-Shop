a9a4f2ca7a1c72dc20075f0a1039642a3f513f9d
SummerJyl <137237074+SummerJyl@users.noreply.github.com>
2025-06-20 14:04:03 -0700

    Updating components, etc.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",    // all JS/TS files in src
+    "./index.html",                  // your main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
